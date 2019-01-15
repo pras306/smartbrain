f7b22bc11b8c12d5e45fe36856f65b3140c9f65c
Prasanna Sriram <pras306@gmail.com>
2019-01-15 16:33:19 +0530

    Resolved width issue on screen size change

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,10 +3,10 @@ import './ImageLinkForm.css'
 
 const ImageLinkForm = ({searchChange, buttonClick}) => {
 	return(
-		<div className='posCenter w-60'>
-			<p className='tc w-60'>This Magic Brain will detect faces in your images. Give it a try</p>
-			<div className='w-60 pa4 br3 shadow-4 form'>
-				<input className='ph2 mh2 w-60 dib' type='text' onChange={searchChange}/>
+		<div className='posCenter w-5'>
+			<p className='tc w-5'>This Magic Brain will detect faces in your images. Give it a try</p>
+			<div className='w-5 pa4 br3 shadow-4 form'>
+				<input className='ph2 mh2 w-5 dib' type='text' onChange={searchChange}/>
 				<button className='ph2 mh2 link dib grow pointer white bg-light-purple' onClick={buttonClick}>Detect</button>
 			</div>			
 		</div>
